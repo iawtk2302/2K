340991a485e36994c573781664b384974804af67
Khanh <bakhanh113ct@gmail.com>
2022-11-23 13:04:51 +0700

    update order

--- lib/model/order.dart
+++ lib/model/order.dart
@@ -4,44 +4,52 @@ class Order {
   String? idOrder;
   String? idUser;
   int? total;
+  String? idAddress;
   String? state;
   String? idVoucher;
   String? note;
   Timestamp? dateCreated;
   Timestamp? dateCompleted;
+  String? dateCanceled;
 
   Order({
     this.idOrder,
     this.idUser,
     this.total,
+    this.idAddress,
     this.state,
     this.idVoucher,
     this.note,
     this.dateCreated,
     this.dateCompleted,
+    this.dateCanceled,
   });
 
   Order.fromJson(Map<String, dynamic> json) {
     idOrder = json['idOrder'];
     idUser = json['idUser'];
+    total = json['idAddress'];
     total = json['total'];
     state = json['state'];
     idVoucher = json['idVoucher'];
     note = json['note'];
     dateCreated = json['dateCreated'];
     dateCompleted = json['dateCompleted'];
+    dateCompleted = json['dateCanceled'];
   }
 
   Map<String, dynamic> toJson() {
     final Map<String, dynamic> data = new Map<String, dynamic>();
     data['idOrder'] = this.idOrder;
     data['idUser'] = this.idUser;
+    data['total'] = this.idAddress;
     data['total'] = this.total;
     data['state'] = this.state;
     data['idVoucher'] = this.idVoucher;
     data['note'] = this.note;
     data['dateCreated'] = this.dateCreated;
     data['dateCompleted'] = this.dateCompleted;
+    data['dateCompleted'] = this.dateCanceled;
 
     return data;
   }
